d9b3258d532392b38bde113b36771b13702e7b9a
hellking1 <51395259+hellking1@users.noreply.github.com>
2019-06-11 21:34:48 +0530

    Update whatshack.sh

--- whatshack.sh
+++ whatshack.sh
@@ -9,8 +9,8 @@ echo "
 
 Copyright (c) 2018 ceweberbatang Ganteng";
 echo "";
-read -p "Nomer Kamu (+62xxxx)=> " nomer;
-read -p "Nomer Target (+62xxxx) => " target;
+read -p "Nomer Kamu (+91xxxx)=> " nomer;
+read -p "Nomer Target (+91xxxx) => " target;
 sleep 2
 echo "[+] Syncing Target...";
 sleep 1
